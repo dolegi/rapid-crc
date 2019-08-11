235c0a7d55428e59b1345bd19f549b90b9e2fb16
Dominic Ginger <5877689+dolegi@users.noreply.github.com>
2019-08-11 13:57:47 +0200

    git ignore build directoty

--- build/Release/.deps/Release/rapid_crc.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/rapid_crc.node := c++ -bundle -undefined dynamic_lookup -Wl,-no_pie -Wl,-search_paths_first -mmacosx-version-min=10.7 -arch x86_64 -L./Release -stdlib=libc++  -o Release/rapid_crc.node Release/obj.target/rapid_crc/src/cc/main.o 
