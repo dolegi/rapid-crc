b9114f4782ca829af91af9416dc5eff6446a8a85
Dominic Ginger <5877689+dolegi@users.noreply.github.com>
2019-08-11 19:42:23 +0200

    Add more crc32c libraries to compare against

--- performance/crc32c.spec.js
+++ performance/crc32c.spec.js
@@ -4,6 +4,9 @@ const { crc32c } = require('../src')
 const { crc32cjs }  = require('../src/js/crc32')
 const createInput = require('../spec/helpers/createInput')
 
+const turbo = require('turbo-crc32/crc32c')
+const sse4 = require('sse4_crc32')
+
 describe('crc32c', () => {
   beforeAll(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
@@ -12,12 +15,10 @@ describe('crc32c', () => {
   it('is the fastest', done => {
     const input = createInput(100);
     (new Benchmark.Suite)
-      .add('rapid', () => {
-        crc32c(input)
-      })
-      .add('rapidjs', () => {
-        crc32cjs(input)
-      })
+      .add('rapid', () => crc32c(input))
+      .add('rapidjs', () => crc32cjs(input))
+      .add('turbo', () => turbo(input))
+      .add('sse4', () => sse4.calculate(input))
       .on('cycle', event => {
         console.log(String(event.target))
       })
