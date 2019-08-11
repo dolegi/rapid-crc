8987a2a048f9c7089ee2dea323d808621ce6c232
Dominic Ginger <5877689+dolegi@users.noreply.github.com>
2019-08-11 17:43:41 +0200

    Add performance tests against popular crc libraries

--- performance/crc32.spec.js
+++ performance/crc32.spec.js
@@ -1,9 +1,13 @@
 const Benchmark = require('benchmark')
 
-const { zlib } = require('../build/Release/rapid_crc.node')
 const { crc32 } = require('../src')
 const { crc32js }  = require('../src/js/crc32')
 const createInput = require('../spec/helpers/createInput')
+
+const { zlib } = require('../build/Release/rapid_crc.node')
+const sheetjs = require('crc-32') 
+const crc = require('crc')
+const turbo = require('turbo-crc32/crc32c')
 
 describe('crc32', () => {
   beforeAll(() => {
@@ -13,15 +17,12 @@ describe('crc32', () => {
   it('is the fastest', done => {
     const input = createInput(100);
     (new Benchmark.Suite)
-      .add('zlib', () => {
-        zlib(input, -1)
-      })
-      .add('rapid', () => {
-        crc32(input)
-      })
-      .add('rapidjs', () => {
-        crc32js(input)
-      })
+      .add('zlib', () => zlib(input, -1))
+      .add('rapid', () => crc32(input))
+      .add('rapidjs', () => crc32js(input))
+      .add('node-crc', () => crc.crc32(input))
+      .add('sheetjs', () => sheetjs.buf(input))
+      .add('turbo', () => turbo(input))
       .on('cycle', event => {
         console.log(String(event.target))
       })
