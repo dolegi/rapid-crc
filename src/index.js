675ccccc2c4118d62581da2b8f84b98590542e17
Dominic Ginger <5877689+dolegi@users.noreply.github.com>
2019-08-11 15:48:55 +0200

    Validate input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const { zlib } = require('../build/Release/rapid_crc.node')
+const { zlib, crc32, crc32c } = require('../build/Release/rapid_crc.node')
+
+function validateInput (fn) {
+	return function (buf, crc = -1) {
+		if (!buf instanceof Buffer) {
+			try {
+				buf = Buffer.from(buf)
+			} catch () {
+				throw Error('Invalid input, expected instance of Buffer')
+			}
+		}
+		if (typeof crc !== 'number') {
+			throw Error('Invalid crc value, expected number')
+		}
+
+		fn(buf, crc);
+	}
+}
 
 module.exports = {
+	validateInput(crc32),
+	validateInput(crc32c)
 
 }
