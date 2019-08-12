0c862f39c417b5557ddd60ea8e6e8e892e06bd87
Dominic Ginger <5877689+dolegi@users.noreply.github.com>
2019-08-12 19:40:58 +0200

    Exclude sse4 in nodejs v12

--- performance/crc32c.spec.js
+++ performance/crc32c.spec.js
@@ -5,28 +5,32 @@ const { crc32cjs }  = require('../src/js/crc32')
 const createInput = require('../spec/helpers/createInput')
 
 const turbo = require('turbo-crc32/crc32c')
-const sse4 = require('sse4_crc32')
 
 describe('crc32c', () => {
-  beforeAll(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
-  })
+	beforeAll(() => {
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+	})
 
-  it('is the fastest', done => {
-    const input = createInput(100);
-    (new Benchmark.Suite)
-      .add('rapid', () => crc32c(input))
-      .add('rapidjs', () => crc32cjs(input))
-      .add('turbo', () => turbo(input))
-      .add('sse4', () => sse4.calculate(input))
-      .on('cycle', event => {
-        console.log(String(event.target))
-      })
-      .on('complete', function () {
-        expect(this.filter('fastest').map('name')[0]).toEqual('rapid')
-        done()
-      })
-      .run({ 'async': true })
-  })
+	it('is the fastest', done => {
+		const input = createInput(100);
+		const suite = (new Benchmark.Suite)
+			.add('rapid', () => crc32c(input))
+			.add('rapidjs', () => crc32cjs(input))
+			.add('turbo', () => turbo(input))
+
+		if (parseInt(process.versions.node) <= 11) {
+			const sse4 = require('sse4_crc32')
+			suite.add('sse4', () => sse4.calculate(input))
+		}
+		suite
+			.on('cycle', event => {
+				console.log(String(event.target))
+			})
+			.on('complete', function () {
+				expect(this.filter('fastest').map('name')[0]).toEqual('rapid')
+				done()
+			})
+			.run({ 'async': true })
+	})
 })
 
